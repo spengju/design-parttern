6d56526ae9bbceba5942be591d24e31a99a06c91
spengju <2658111307@qq.com>
2024-10-13 17:22:58 +0800

    注释

--- src/main/java/com/peng/pattern/abstractFactory/Test.java
+++ src/main/java/com/peng/pattern/abstractFactory/Test.java
@@ -6,7 +6,7 @@ import com.peng.pattern.abstractFactory.impl.MysqlDb;
  * @Author: spengju
  * @Slogan: Day day no bug.
  * @Date: 2024/10/13 16:52
- * @Desc:
+ * @Desc: 抽象工厂模式：提供一个创建一系列相关或互相依赖对象的接口，而无需指定它们具体的类
  */
 public class Test {
     public static void main(String[] args) {
